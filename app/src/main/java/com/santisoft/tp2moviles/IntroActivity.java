3d54fe5a89a7574eaaaf4b4a926456ff5a83f073
Fermin2049 <fermin2049@gmail.com>
2024-08-28 19:55:05 -0300

    Intro Activity

---
+++ app/src/main/java/com/santisoft/tp2moviles/IntroActivity.java
@@ -0,0 +1,33 @@
+package com.santisoft.tp2moviles;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.santisoft.tp2moviles.databinding.ActivityIntroBinding;
+
+public class IntroActivity extends AppCompatActivity {
+ActivityIntroBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+
+            }
+        });
+    }
+}
