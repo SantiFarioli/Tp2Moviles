4ef485d0e603439403c14aec035a00c4d68f3fbb
alex-astudillo <105954347+ast-alex@users.noreply.github.com>
2024-08-29 20:18:24 -0300

    l3

--- app/src/main/java/com/santisoft/tp2moviles/DetalleAtivityViewModel.java
+++ app/src/main/java/com/santisoft/tp2moviles/DetalleAtivityViewModel.java
@@ -10,9 +10,7 @@ import androidx.lifecycle.MutableLiveData;
 
 import com.santisoft.tp2moviles.modelo.Libro;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 
 public class DetalleAtivityViewModel extends AndroidViewModel {
     private final ArrayList<Libro> listaLibros = new ArrayList<>();
@@ -20,8 +18,11 @@ public class DetalleAtivityViewModel extends AndroidViewModel {
 
     public DetalleAtivityViewModel(@NonNull Application application) {
         super(application);
-        listaLibros.add(new Libro("Martin Fierro","José Hernández",1872,"Poesia","El libro “El Gaucho Martín Fierro” es un poema narrativo escrito en verso por José Hernández en 1872. Se considera una obra literaria ejemplar del género gauchesco y tiene 2316 versos distribuidos en 13 cantos.", R.drawable.martin_fierro_1894));
-        listaLibros.add(new Libro("Ficciones", "Jorge Luis Borges",1944,"ficción literaria","El libro se divide en dos secciones llamadas El jardín de senderos que se bifurcan y Artificios. La división del libro en dos se debe a que la primera parte había sido publicada originalmente tres años antes, en 1941, como un libro individual.", R.drawable.ficciones));
+        listaLibros.add(new Libro("Martin Fierro","José Hernández",1872,"Poesia","El libro “El Gaucho Martín Fierro” es un poema narrativo escrito en verso por José Hernández en 1872. Se considera una obra literaria ejemplar del género gauchesco y tiene 2316 versos distribuidos en 13 cantos.", R.drawable.martin_fierro));
+        listaLibros.add(new Libro("Ficciones", "Jorge Luis Borges",1944,"ficcin literaria","El libro se divide en dos secciones llamadas El jardín de senderos que se bifurcan y Artificios. La división del libro en dos se debe a que la primera parte había sido publicada originalmente tres años antes, en 1941, como un libro individual.", R.drawable.ficciones));
+        listaLibros.add(new Libro("El Aleph","Jorge Luis Borges",1945,"Cuento","El Aleph, escrito por Jorge Luis Borges en 1945, es una colección de diecisiete cuentos que exploran temas como la infinitud, la identidad, el tiempo y el espacio. El libro se considera uno de los más emblemáticos de la literatura fantástica latinoamericana y ha sido ampliamente analizado y estudiado.", R.drawable.aleph));
+        listaLibros.add(new Libro("Rayuela", "Julio Cortázar",1963,"Ficción","Rayuela es una de las novelas más innovadoras y reconocidas de la literatura latinoamericana. Cortázar ofrece al lector la opción de leer la novela en varios órdenes, lo que permite múltiples interpretaciones y experiencias de la historia. Es un libro que explora la existencia humana, el amor, y la búsqueda de sentido en un mundo complejo.",R.drawable.rayuela));
+
 
     }
 
