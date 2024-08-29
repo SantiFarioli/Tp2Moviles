6f5a57f55bc70f3bb325b3c9d8a5079f39a959ac
Fermin2049 <fermin2049@gmail.com>
2024-08-29 19:31:57 -0300

    arreglo error

--- app/src/main/java/com/santisoft/tp2moviles/DetalleAtivityViewModel.java
+++ app/src/main/java/com/santisoft/tp2moviles/DetalleAtivityViewModel.java
@@ -18,7 +18,7 @@ public class DetalleAtivityViewModel extends AndroidViewModel {
     private final ArrayList<Libro> listaLibros = new ArrayList<>();
     private final MutableLiveData<Libro> mLibro = new MutableLiveData<>();
 
-    public DetalleAtivityViewModel(@NonNull Application application, MutableLiveData<Libro> mLibro) {
+    public DetalleAtivityViewModel(@NonNull Application application) {
         super(application);
         listaLibros.add(new Libro("Martin Fierro","José Hernández",1872,"Poesia","El libro “El Gaucho Martín Fierro” es un poema narrativo escrito en verso por José Hernández en 1872. Se considera una obra literaria ejemplar del género gauchesco y tiene 2316 versos distribuidos en 13 cantos.", R.drawable.martin_fierro_1894));
     }
